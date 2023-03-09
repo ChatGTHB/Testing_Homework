75003de6ba3f0e55e404ac73cd13182d1ddc851a
chtgthb <alwaysjusticeforall@gmail.com>
2023-03-10 00:03:56 +0300

    continue

--- src/homework/_06_Selenium_08_03_2023/Tests.java
+++ src/homework/_06_Selenium_08_03_2023/Tests.java
@@ -106,7 +106,40 @@ public class Tests extends BaseDriver {
         MyFunc.bekle(5);
         js.executeScript("arguments[0].click();", video80);
         MyFunc.bekle(5);
-
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* sdftqertwe45tywwtekdfpohklsbh
+* agjkdfplgjopsmg
+*
+*
+*
+*
+* drlgpğsdlrgp
+* edgoksdopfgbk
+* posdfjfkgop*/
     }
 }
 
